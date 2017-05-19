c4d5dba3d7bd0301c88fb920fbc2673a57de838d
Anton Legoo <azeven@gmail.com>
2017-05-19 14:45:45 -0700

    Added view link option, cleanup.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 var copy            = require( "copy" );
 
-module.exports = {
+module.exports = 
+{
     // Extend website resources and html
-    website: {
-        // assets: "./book",
-        // js: [
-        //     "test.js"
-        // ],
-        // css: [
-        //     "test.css"
-        // ]
-    },
+    // website: 
+    // {
+    //     assets: "./book",
+    //     js: [
+    //         "test.js"
+    //     ],
+    //     css: [
+    //         "test.css"
+    //     ]
+    // },
 
     // Extend templating blocks
-    blocks: {
-        
-        pdf: {
-            process: function(block) {
-                
+    blocks: 
+    {    
+        pdf: 
+        {
+            process: function(block) 
+            {
                 var book = this;
 
                 // Return nothing and warn if there's no source
@@ -25,15 +28,33 @@ module.exports = {
                 {
                     book.log.warn.ln( `Embed-pdf: no src attribute in pdf tag in file ${this.ctx.ctx.file.path}.` );
                     return ``;
-                } 
+                }
+
+                // Construct URL
+                var url = `${block.kwargs.src}`;
+
+                // Construct the width/height
+                var width = (block.kwargs.width) ? `width="${block.kwargs.width}"` : "";
+                var height = (block.kwargs.width) ? `height="${block.kwargs.height}"` : "";
+
+                // Construct download link
+                var viewLink = ( block.kwargs.link ||  block.kwargs.link == undefined ) ? `
+                <div class="pdf__link"><a target="_blank" href="${url}">View PDF</a></div>
+                ` : "";
 
                 // Construct the return html
-                var html = `<embed class="embed-pdf" src="${block.kwargs.src}"`;
-                    // Add the width if passed
-                    html+= (block.kwargs.width) ? ` width="${block.kwargs.width}"` : "";
-                    // Add the height if passed
-                    html+= (block.kwargs.height) ? ` height="${block.kwargs.height}"` : "";
-                    html+= ` type='application/pdf'>`;
+                var html =  `<div class="pdf">
+                                ${viewLink}
+                                <object data="${url}" ${width} ${height} type="application/pdf">
+                                    <embed src="${url}">
+                                        <p>
+                                            This browser does not support PDFs. <br/>
+                                            Please download the PDF to view it: <a target="_blank" href="${url}">Download PDF</a>.
+                                        </p>
+                                    </embed>
+                                </object>
+                            </div>`;
+
                 // Return the html
                 return html;
             }
@@ -41,16 +62,18 @@ module.exports = {
     },
 
     // Hook process during build
-    hooks: {
-        "init": function() {
-
+    hooks: 
+    {
+        "init": function() 
+        {
             var book = this;
 
             // Get the output assets dir
             var output_dir = `${book.output.root()}/assets`;
 
             // Copy all PDF's in assets to the build dir's assets
-            copy('assets/**/*.pdf', output_dir, function(err, files) {
+            copy('assets/**/*.pdf', output_dir, function(err, files)
+            {
                 // Handle error
                 if (err) throw err;
                 // Log
